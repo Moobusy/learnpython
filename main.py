a9a35dec18fcdc4238e83f881c6e308667ec7029
Moomesy <moobusy@qq.com>
2015-05-03 23:32:11 +0800

    add TestSetting and LocalSetting

--- main.py
+++ main.py
@@ -4,7 +4,12 @@
 from app import create_app
 
 if __name__ == '__main__':
-	server_host = '192.168.31.110'
-	server_port = 80
+#	本地测试环境
+	local_host = '192.168.31.110'
+	local_port = 80
+#	公开测试环境
+	test_host = '127.0.0.1'
+	test_port = 5000
+	
 	app = create_app()
-	app.run(host=server_host, port=server_port)
+	app.run(host=test_host, port=test_port)
